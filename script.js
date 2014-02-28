82c3e34c2a24d8e41117ff70fe388f07212f5774
M K <mirandak@Miranda-Kushner.local>
2014-02-27 16:26:51 -0800

    random cats

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var images = ["images/0.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg"]$(document).ready(function() {    $("body").append("<img src='images/0.jpg'/>")}); 
+var images = ["images/0.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg"]var current = Math.floor(Math.random() * 4);$(document).ready(function() {    $("body").append("<img src='images/" + current + ".jpg'/>")}); 
