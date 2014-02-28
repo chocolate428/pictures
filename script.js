20ea5cebe61559fd91bbdac99cf2a6b43fe2588d
M K <mirandak@Miranda-Kushner.local>
2014-02-27 16:59:21 -0800

    things

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-var images = ["images/0.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg"]var current = Math.floor(Math.random() * 4);$(document).ready(function() {    $("body").append("<img src='images/" + current + ".jpg'/>")}); 
+var getCat = function(){var images = ["images/0.jpg", "images/1.jpg", "images/2.jpg", "images/3.jpg"]var current = Math.floor(Math.random() * 4);$("body").append("<img src='images/" + current + ".jpg'/>")}$(document).ready(function() {    $(".button").click(function(){		$("img").remove();	getCat();})     getCat();})
